3da753757180459e55bafe68066a084ee8003ce7
ahmed-ba <elbahrini.ahmed@gmail.com>
2023-12-03 15:52:50 +0100

    3

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 sudo npm cache clean -f
+
+sudo npm install 
 sudo chown -R 501:20 "/Users/runner/.npm"
-sudo npm install -g n
 sudo n stable
-sudo npm install yarn -g
+sudo npm install 
 sudo npm install -g ionic
 cd ../../
 npm i
