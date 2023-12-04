4607bc097a13a4b5c63d561ac1175a690ff56ef1
ahmed-ba <elbahrini.ahmed@gmail.com>
2023-12-04 06:55:50 +0100

    09

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
-sudo npm cache clean -f
 
-sudo npm install 
-sudo chown -R 501:20 "/Users/runner/.npm"
-sudo n stable
-sudo npm install 
-sudo npm install -g ionic
-cd ../../
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+# Required nodeJS version
+NODE_VERSION=10.17.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node10 "$NODE_VERSION"
+
+# go to root of project
+cd ../..
+
+# install dependencies
 npm i
-ionic build
-./gradlew clean
-./gradlew assemble
+
+# run optimized production build
+npm run build -- --prod
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
 npx cap sync
